92c5fcca9177b43828699369e9e8389dafa247a8
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-07 13:42:00 -0400

    revert to challenge day 1

--- client.js
+++ client.js
@@ -1,53 +1,33 @@
-console.log('There is no try, only do')
+console.log("hello world!")
 
-//create variables for page elements
+//let number = 3
+//create variable for button element
 let $button = document.getElementById('addTodo')
 let $textBox = document.getElementById('userInput')
 let $list = document.getElementById('list')
-let $completed = document.getElementById('completed')
-//add click event handler on button
+
+//set function as click handler
 $button.onclick = addTodo
 
-//define the button click handler
-function addTodo(event) {
-    console.log("button was clicked")
+function addTodo() {
+    console.log("button clicked")
     $button.style.backgroundColor = 'red'
-
-    //get text from textBox
+    
+    //get text from textbox
     let newTodoText = $textBox.value
-    console.log(newTodoText)
-    //reset textBox
-    $textBox.value = ''
     //create new list item
-    let $newTodoItem = document.createElement('li')
-    //use String template with backticks
-    $newTodoItem.innerHTML =
-        `${newTodoText} &nbsp;
-        <button onclick='taskDone(event)'>Done</button> &nbsp;
-        <button onclick='deleteTask(event)'>Delete</button>`
+    let $newTodo = document.createElement('li')
+    $newTodo.innerHTML = newTodoText
     //put list item in list
-    $list.append($newTodoItem)
+    $list.append($newTodo)
 }
 
-function taskDone(event) {
-    //event.target is the button that was clicked
-    let $listItem = event.target.parentElement
-    console.log($listItem)
-    event.target.remove()
-    $listItem.style.textDecoration = 'line-through'
-    $completed.append($listItem)
-}
+//variable for h1
 
-function deleteTask(event) {
-    event.target.parentElement.remove()
-}
+//set changeColor function as click handler
 
-//1. variable for heading element
-let $head = document.querySelector('h1')
-//2. set 'changeColor' function as click handler on heading
-$head.onclick = changeColor
-//3.a define 'changeColor' function
+//define changeColor function
 function changeColor() {
-    //3.b. set color (text) of heading to red
-    $head.style.color = 'red'
+    //set color (Text) of h1 to yellow
+
 }
